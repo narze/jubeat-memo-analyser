159fc9b9bb185544747a7bba7e70ec3c0828cf8d
Manassarn Manoonchai <manassarn@gmail.com>
2012-06-06 18:50:29 +0700

    Conversion of simple jubeat memo
    Merge
    
    merge

--- jubeat_memo_analyser.rb
+++ jubeat_memo_analyser.rb
@@ -2,65 +2,60 @@
 # encoding: utf-8
 # Converts jubeat memo to jubeat analyser format
 
-input = nil, output = nil
-blank = '□'
-one = '①'
-front = []
-back = []
-blankBack = '－'
-numberSymbols = ['①', '②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨', '⑩', '⑪', '⑫', '⑬', '⑭', '⑮', '⑯', '⑰', '⑱', '⑲', '⑳']
-jubeatAnalyserSymbols = ['@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S']
-prefix = '‡'
+input = ""
+memoBlank = '□'
+memoBackBlank = '－'
+analyserSymbols = ['@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+analyserSymbolPrefix = '‡'
 space = " "
-groups = []
-group = []
-out = []
+groups = Array.new
+group = Array.new
+out = Array.new
 
 ARGV.each_with_index do |item, i|
   input = item if i == 0
-  output = item if i == 1
 end
+
+abort("ERROR : No input specified") if input.empty?
+abort("ERROR : Input not found") if !File.exists?(input)
 
 File.foreach(input, :encoding => "UTF-8") do |line|
   if /^\d+$/.match(line)
     groups.push group
     group = Array.new
-    # puts "number #{line}"
-  elsif /^.{4}\s+.+$/.match(line)
+  elsif /^.{4}\s+/.match(line)
     group.push line
-    # puts line
   end
 end
 
-#push last group if group is not empty
+# push last group if group is not empty
 groups.push group unless group.empty?
 
-#flush first element
+# flush first empty line
 groups.shift
 
-# puts groups
-
+# convert into jubeat analyser format
 groups.each_with_index do |a_group, index|
-  front = []
-  back = []
+  front = Array.new
+  back = Array.new
   a_group.each do |line|
     front.concat line[0..3].split('')
     backside = line[/\|(.+)\|/, 1]
     back.concat(backside.split('')) unless backside.nil?
   end
 
-  #replace with jubeat analyser symbols
+  # replace with jubeat analyser symbols
   back.each_with_index do |item, i|
-    if item != blankBack
+    if item != memoBackBlank
       front.map! do |x|
-        x == item ? "#{prefix}#{jubeatAnalyserSymbols[i]}" : x
+        x == item ? "#{analyserSymbolPrefix}#{analyserSymbols[i]}" : x
       end
     end
   end
 
   #fill space
   front.map! do |x|
-    x == blank ? "#{prefix}#{space}" : x
+    x == memoBlank ? "#{analyserSymbolPrefix}#{space}" : x
   end
 
   out.push "---------- #{index+1}"
@@ -68,10 +63,13 @@ groups.each_with_index do |a_group, index|
     break if shifted.empty?
     out.push shifted.join
   end
-
 end
 
-f = File.new(output, 'w')
+# write result
+# Set encoding to windows 1252
+# (jubeat analyser cannot read utf-8)
+output = File.basename(input, ".*") + "_jma.txt"
+f = File.new(output, 'w:Windows-1252')
 out.each do |line|
   f.write("#{line}\n");
 end
